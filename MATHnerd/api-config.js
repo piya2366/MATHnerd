a5acce039e215becc6082f0613826c7333490b26
piya2366 <priyashukla4012@gmail.com>
2025-04-13 08:37:33 +0000

    Add files via upload

---
+++ MATHnerd/api-config.js
@@ -0,0 +1,7 @@
+// config/api-config.js
+const API_CONFIG = {
+    DEEPSEEK_API_KEY: "sk-872e44cd2a3d44deae80012580ea3fcc",
+    DEEPSEEK_API_URL: "https://api.deepseek.com/v1/chat/completions"
+};
+
+// Make sure this file is added to your .gitignore!
